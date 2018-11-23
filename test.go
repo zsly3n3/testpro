62dff32ee2fd84e746af02e0c50ee5e28bfe0010
wmj <work@lmm.com>
2018-11-23 11:42:06 +0800

    add

--- test.go
+++ test.go
@@ -9,8 +9,8 @@ import (
 func main() {
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
-		for {
-			fmt.Println("a")
+		for i := 0; i < 1000000; i++ {
+			fmt.Println(fmt.Sprintf("%d", i))
 		}
 	})
 	r.Run("127.0.0.1:9995") // listen and serve on 0.0.0.0:8080
